1a2039134ee2b4951e3695241214f8cff7adec2f
RomanCzujko <roman.chujko@gmail.com>
2020-07-15 11:45:42 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://theme536-full/admin/');
+define('HTTP_CATALOG', 'http://theme536-full/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://theme536-full/admin/');
+define('HTTPS_CATALOG', 'http://theme536-full/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/admin/');
+define('DIR_SYSTEM', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/');
+define('DIR_LANGUAGE', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/admin/language/');
+define('DIR_TEMPLATE', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/admin/view/template/');
+define('DIR_CONFIG', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/config/');
+define('DIR_IMAGE', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/image/');
+define('DIR_CACHE', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/cache/');
+define('DIR_DOWNLOAD', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/download/');
+define('DIR_UPLOAD', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/upload/');
+define('DIR_LOGS', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/logs/');
+define('DIR_MODIFICATION', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/modification/');
+define('DIR_CATALOG', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://theme536-full/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://theme536-full/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/catalog/');
+define('DIR_SYSTEM', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/');
+define('DIR_LANGUAGE', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/catalog/language/');
+define('DIR_TEMPLATE', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/config/');
+define('DIR_IMAGE', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/image/');
+define('DIR_CACHE', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/cache/');
+define('DIR_DOWNLOAD', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/download/');
+define('DIR_UPLOAD', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/upload/');
+define('DIR_MODIFICATION', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/modification/');
+define('DIR_LOGS', 'C:\Users\Roman\Downloads\OSPanel\domains\theme536-full/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
